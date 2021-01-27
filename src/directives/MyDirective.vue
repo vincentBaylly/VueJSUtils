9873408ef1d63632f078d737428ae74a582f84d4
Vincent Baylly <vincent.baylly@gmail.com>
2021-01-26 21:31:36 -0500

    add global mixin and custom directive

---
+++ src/directives/MyDirective.vue
@@ -0,0 +1,19 @@
+<script>
+import Vue from "vue";
+
+Vue.directive("my-directive", {
+  bind: function() {
+    // faire un travail de préparation
+    // par exemple. ajouter des écouteurs d'événements ou des trucs coûteux
+    // qui ne doit être exécuté qu'une seule fois
+  },
+  update: function(newValue, oldValue) {
+    // faire quelque chose en fonction de la valeur mise à jour
+    // cela sera également appelé pour la valeur initiale
+  },
+  unbind: function() {
+    // do clean up work
+    // e.g. remove event listeners added in bind()
+  },
+});
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+//declaration global de mixin
+Vue.mixin({
+  mounted() {
+    console.log("I am mixed into every component.");
+  },
+});
+
+//Déclaration globale d'une directive
+// Vue.directive("mydirective", {
+//   inserted: function(el) {
+//   },
+// });
+
+Vue.directive("highlight", {
+  bind: function(el, binding) {
+    el.style.background = binding.value;
+  },
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
