31652bd8e1df11b352c0d13ed1bed40fb2711fbf
Vincent Baylly <vincent.baylly@gmail.com>
2021-01-26 21:30:50 -0500

    remove HelloWord + add myComponent

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p v-highlight="'yellow'">Highlight this text bright yellow</p>
+  </div>
+</template>
+
+<script>
+import { MyMixin } from "../mixins/MyMixin";
+
+export default {
+  mixins: [MyMixin],
+  name: "MyComponent",
+  //Ordre de priorite
+  created() {
+    //console.log("Affichage du message apres le mixin");
+  },
+  //Gestion des conflits de nom de propriete
+  // data() {
+  //   return {
+  //     message: "I take priority.",
+  //   };
+  // },
+  // gestion des conflits de nom de methode
+  methods: {
+    // hello() {
+    //   console.log("Hello from the component.");
+    // },
+  },
+};
+</script>
+
+<style scoped></style>
