0452d17dd46eaaa1710c50f373a426a4963f5dc6
Vincent Baylly <vincent.baylly@gmail.com>
2021-01-26 21:33:48 -0500

    VueJS Mixin example

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input :value="value" @input="updateValue" v-bind="$attrs" />
+  </div>
+</template>
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    value: [String, Number],
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit("input", event.target.value);
+    },
+  },
+};
+</script>
+<!--template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input
+      :value="value"
+      @input="updateValue"
+      v-bind="$attrs"
+      v-on="listeners"
+    />
+  </div>
+</template-->
+<script>
+// import { formFieldMixin } from "../mixins/formFieldMixin"; // import mixin
+// export default {
+//   mixins: [formFieldMixin], // register mixin
+//   props: {
+//     value: [String, Number],
+//   },
+//   computed: {
+//     listeners() {
+//       return {
+//         ...this.$listeners,
+//         input: this.updateValue,
+//       };
+//     },
+//   },
+// };
+</script>
---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <select
+      :value="value"
+      @change="updateValue"
+      v-bind="$attrs"
+      v-on="$listeners"
+    >
+      <option
+        v-for="option in options"
+        :value="option"
+        :key="option.id"
+        :selected="option === value"
+        >{{ option }}</option
+      >
+    </select>
+  </div>
+</template>
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    value: [String, Number],
+    label: String,
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit("input", event.target.value);
+    },
+  },
+};
+</script>
+<!--template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <select
+      :value="value"
+      @change="updateValue"
+      v-bind="$attrs"
+      v-on="$listeners"
+    >
+      <option
+        v-for="option in options"
+        :value="option"
+        :key="option.id"
+        :selected="option === value"
+        >{{ option }}</option
+      >
+    </select>
+  </div>
+</template-->
+<script>
+// import { formFieldMixin } from "../mixins/formFieldMixin"; // import mixin
+// export default {
+//   mixins: [formFieldMixin], // register mixin
+//   props: {
+//     options: {
+//       type: Array,
+//       required: true,
+//     },
+//     value: [String, Number],
+//   },
+// };
+</script>
